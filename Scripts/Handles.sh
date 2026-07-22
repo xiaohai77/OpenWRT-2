7b3bce29035d269b242b19d7bfb09c4b4df3af9a
第十六夜月 <mashanghai88@gmail.com>
2026-07-23 03:06:08 +0800

    Update menu paths for luci-app-openlist

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -221,13 +221,13 @@ if [ -d "$PKG_PATH/luci-app-mini-diskmanager" ]; then
 	fi
 fi
 
-#修改openlist2菜单位置到nas
-if [ -d "luci-app-openlist2" ]; then
-	echo " " && cd ./luci-app-openlist2/
+#修改openlist菜单位置到nas
+if [ -d "luci-app-openlist" ]; then
+	echo " " && cd ./luci-app-openlist/
 
-	sed -i "s/services/nas/g" ./luci-app-openlist2/root/usr/share/luci/menu.d/luci-app-openlist2.json
+	sed -i "s/services/nas/g" ./luci-app-openlist/root/usr/share/luci/menu.d/luci-app-openlist.json
 
-	cd $PKG_PATH && echo "openlist2 menu has been fixed!"
+	cd $PKG_PATH && echo "openlist menu has been fixed!"
 fi
 
 #修改qbittorrent菜单位置到nas
