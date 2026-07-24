18c01305a4e3bdd4f37d14d207448da5a81c9199
第十六夜月 <mashanghai88@gmail.com>
2026-07-24 22:17:52 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -47,7 +47,7 @@ UPDATE_PACKAGE() {
 # UPDATE_PACKAGE "open-app-filter" "destan19/OpenAppFilter" "master" "" "luci-app-appfilter oaf" 这样会把原有的open-app-filter，luci-app-appfilter，oaf相关组件删除，不会出现coremark错误。
 
 # 自定义
-UPDATE_PACKAGE "singbox" "XiaoHaiSly/OpenWrt-SingBox" "main"
+UPDATE_PACKAGE "singbox" "XiaoHaiSly/OpenWrt-SingBox" "beta"
 UPDATE_PACKAGE "substore" "XiaoHaiSly/OpenWrt-SubStore" "main"
 UPDATE_PACKAGE "miaomiaowu" "XiaoHaiSly/OpenWrt-MMW" "main"
 UPDATE_PACKAGE "adguardhome" "XiaoHaiSly/luci-app-adguardhome" "main"
